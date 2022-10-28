8cbfd3644de0e4e671e165b8ead01fddb23ee480
ceyzaguirre4 <ceyzaguirre4@uc.cl>
2022-10-28 11:59:48 -0700

    bool = False arguments are now passed explicitly in the slurm script

--- test_tube/hpc.py
+++ test_tube/hpc.py
@@ -378,7 +378,7 @@ class SlurmCluster(AbstractCluster):
             v = trial.__dict__[k]
 
             # don't add None params
-            if v is None or v is False:
+            if v is None: # or v is False:
                 continue
 
             # put everything in quotes except bools
